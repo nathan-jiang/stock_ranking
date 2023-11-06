524412f0fa7478e1e9d09aef216b444df4717517
Nan Jiang <bluesnj@gmail.com>
2023-11-06 14:34:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import streamlit_lottie
 from streamlit_lottie import st_lottie
 from streamlit_option_menu import option_menu
-from streamlit_gsheets import GSheetsConnection
 import plotly.graph_objects as go
 import calendar
 import datetime
