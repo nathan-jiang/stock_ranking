270d13f83cdc1078f75ca76bcae7ce46afb7af63
Nan Jiang <bluesnj@gmail.com>
2024-09-05 11:58:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ with st.container():
                         # Show the bar chart
                         st.plotly_chart(fig_bar, use_container_width=True)
                   if st.button("Clear Results"):
-                    result_placeholder.empty()
+                      result_placeholder.empty()
 
     with right_column:
         st_lottie(lottie_stock, height=300, key="stock")
