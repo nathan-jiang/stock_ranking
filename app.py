cbc7e4da79e2dd817add60cb3ad98fd2bfa2b6ab
Nan Jiang <bluesnj@gmail.com>
2024-09-06 10:51:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from streamlit_lottie import st_lottie
 from streamlit_option_menu import option_menu
 import calendar
 
+st.write('#')
 # Streamlit page config
 st.set_page_config(page_title="Ranking Algo on Stocks",
                    page_icon=":tada:",
